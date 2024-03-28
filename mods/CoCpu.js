59648b4d36f3ff39c8b5e56ce62096e1d42da7dc
CarbonMonoxida <164758530+CarbonMonoxida@users.noreply.github.com>
2024-03-28 13:58:10 +0700

    Update CoCpu.js
    Changelog
    -add co detector (hope no bug)
    -fix the cpu cant turn onto computer
    Hope the Fix can Make this Mod better

--- mods/CoCpu.js
+++ mods/CoCpu.js
@@ -20,7 +20,11 @@ elements.carbon_monoxide = {
                     "head": { elem2:"rotten_meat", chance:0.5 },
                     "body": { elem2:"rotten_meat", chance:0.5 },
                     "human": { elem2:"rotten_meat", chance:0.5 },
-                 }
+                    "bird": { elem2:"rotten_meat", chance:0.2 },
+                    "ant": {elem2:"dead_bug", },
+                    "plant": {elem1:"null", },
+                    "meat": {elem2:"rotten_meat", },
+                }
 };
 elements.liquid_carbon_monoxide = {
          color: "#b5b5b5",
@@ -54,8 +58,7 @@ elements.cpu = {
          stateHigh: "explosion",
 reactions: {
                     "virus": { elem1 : null , elem2:"malware", chance:0.9 },
-                    "battery": { elem2:"computer" },
-                    
+                     
                  }
 };
 
@@ -67,10 +70,19 @@ elements.computer = {
          density: 8908,
          tempHigh: 1414,
          stateHigh: "explosion",
-         conduct: 1,
  reactions: {
                     "virus": { elem1 : null , elem2:"malware", chance:0.9 },
                     "water": { elem1: null , elem2: "electric" },                    
+                 }
+elements.co_detector = {
+         color: "#2b2b2a",
+         behavior: behaviors.WALL,
+         category: "machines",
+         state: "solid",
+         density: 8908,
+ reactions: {
+                    "carbon_monoxide": { elem2:electric, chance:0.9 },
+                    "water": { elem1: "explosion", elem2: "electric" },                    
                  }
 };
 
